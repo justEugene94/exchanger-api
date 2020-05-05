92adbd9fe0a93b23b8f4e96ed7e50530e78ce750
justEugene94 <drydude77@gmail.com>
2020-05-05 18:19:03 +0300

    Added create method for Purchases app

--- src/exchange_office/apps/purchases/serializers.py
+++ src/exchange_office/apps/purchases/serializers.py
@@ -1,23 +1,43 @@
 from rest_framework import serializers
 
-from .models import Purchase
+from .models import Purchase, Customer
+from currencies.models import Currency
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+
+    first_name = serializers.CharField(required=True, max_length=20)
+    last_name = serializers.CharField(required=True, max_length=20)
+    phone_number = serializers.CharField(required=True, max_length=20)
+
+
+    class Meta:
+        model = Customer
+        fields = ('id', 'first_name', 'last_name', 'phone_number')
 
 
 class PurchaseSerializer(serializers.ModelSerializer):
 
     value = serializers.IntegerField(required=True)
-
-    customer = serializers.CharField(required=True)
-
+    customer = CustomerSerializer(required=True)
     currency = serializers.CharField(required=True)
-
     exchange_currency = serializers.CharField(required=True)
 
     def create(self, validated_data):
-        super().create(**validated_data)
 
-        return Purchase.objects.create(**validated_data)
+        if 'customer' in validated_data:
+            customer = validated_data['customer']
 
+            customer_model = Customer.objects.filter(phone_number=customer['phone_number']).first()
+
+            if not customer_model:
+                customer_model = Customer.objects.create(**customer)
+
+            validated_data['customer'] = customer_model
+            validated_data['currency'] = Currency.objects.filter(name=validated_data['currency']).first()
+            validated_data['exchange_currency'] = Currency.objects.filter(name=validated_data['exchange_currency']).first()
+
+            return Purchase.objects.create(**validated_data)
 
     class Meta:
         model = Purchase
--- src/exchange_office/apps/purchases/views.py
+++ src/exchange_office/apps/purchases/views.py
@@ -1,5 +1,8 @@
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication, TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import permission_classes, authentication_classes, api_view
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Purchase
 from .serializers import PurchaseSerializer
@@ -13,9 +16,23 @@ class PurchaseView(APIView):
         self.queryset = Purchase.objects.all()
         self.serializer_class = PurchaseSerializer
 
+    # @api_view(['GET'])
+    # @authentication_classes([TokenAuthentication])
+    # @permission_classes([IsAuthenticated])
     def get(self, request):
 
         return Response(self.serializer_class(self.queryset, many=True).data)
 
     def post(self, request):
-        pass
+
+        serializer = self.serializer_class(data=request.data)
+
+        # if serializer.is_valid():
+        #     return Response({'data': serializer.data()})
+
+        if serializer.is_valid():
+            purchase = serializer.save()
+
+            return Response(self.serializer_class(purchase).data)
+
+        return Response({'error': serializer.is_valid(raise_exception=True)})
